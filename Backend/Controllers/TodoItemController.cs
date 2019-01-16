6b62858a27827912116dcd37d12562ea577f4514
Ash P <ashpeterson177@gmail.com>
2019-01-16 18:10:57 +0000

    Add project files.

---
+++ Backend/Controllers/TodoItemController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using Backend.DataObjects;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    /*-------------------------------------------------------------------------------------------------
+     The TableController is the central processing for the database access layer. 
+     It handles all the OData capabilities for us and exposes these as REST endpoints within our WebAPI. 
+     This means that the actual code for this controller is tiny - just 12 lines of code.
+
+     OData is a specification for accessing table data on the Internet. It provides a mechanism for 
+    querying and manipulating data within a table. Entity Framework is a common data access layer for 
+    ASP.NET applications.
+    ------------------------------------------------------------------------------------------------- */
+
+    public class TodoItemController : TableController<TodoItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            MobileServiceContext context = new MobileServiceContext();
+            DomainManager = new EntityDomainManager<TodoItem>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<TodoItem> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<TodoItem> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
+        {
+            TodoItem current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
