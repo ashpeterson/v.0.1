864de762502bc0c9b9c7a15a6a45fcaf60a0fb8e
Ash P <ashpeterson177@gmail.com>
2019-04-16 12:43:10 +0100

    Relationship/Messages

---
+++ Backend/DataObjects/Messages.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace Backend.DataObjects
+{
+    public class Messages : EntityData
+    {
+        public string UserId { get; set; }
+        public string Text { get; set; }
+    }
+}
